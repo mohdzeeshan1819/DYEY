83aa7686e550172e0bc16d3829fd5ff33bcb50a1
Neha <148842693+mohdzeeshan1819@users.noreply.github.com>
2024-01-24 18:35:44 +0530

    validation done

---
+++ app/src/main/java/com/example/dyey/loginFolder/AddPhotos.kt
@@ -0,0 +1,12 @@
+package com.example.dyey.loginFolder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dyey.R
+
+class AddPhotos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photos)
+    }
+}
--- app/src/main/java/com/example/dyey/loginFolder/UploadID.kt
+++ app/src/main/java/com/example/dyey/loginFolder/UploadID.kt
@@ -1,12 +1,26 @@
 package com.example.dyey.loginFolder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dyey.R
+import com.example.dyey.databinding.ActivitySignUpBinding
+import com.example.dyey.databinding.ActivityUploadIdBinding
 
 class UploadID : AppCompatActivity() {
+    private lateinit var binding : ActivityUploadIdBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload_id)
+        binding = ActivityUploadIdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.back.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+            finish()
+        }
+        binding.skip.setOnClickListener {
+            startActivity(Intent(this,AddPhotos::class.java))
+            finish()
+        }
     }
 }
